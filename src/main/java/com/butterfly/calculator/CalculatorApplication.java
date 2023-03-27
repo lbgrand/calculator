fca4e7d8bc6253c64c2be3a1d6ae40fc8e4afdbb
Andrei <gordeevandreymail@gmail.com>
2023-03-27 21:47:57 +0200

    add more stuff

--- src/main/java/com/butterfly/calculator/CalculatorApplication.java
+++ src/main/java/com/butterfly/calculator/CalculatorApplication.java
@@ -2,14 +2,12 @@ package com.butterfly.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+    SpringApplication.run(CalculatorApplication.class, args);
 	}
 
 }
--- src/main/java/com/butterfly/calculator/controller/MainPageController.java
+++ src/main/java/com/butterfly/calculator/controller/MainPageController.java
@@ -1,7 +1,9 @@
 package com.butterfly.calculator.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainPageController {
 
   @GetMapping("/")
