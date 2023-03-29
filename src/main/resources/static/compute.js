d6cc66f1987a313c82ee8ddd1a5f41cf1cedd294
Andrei <gordeevandreymail@gmail.com>
2023-03-29 09:57:36 +0200

    frontend

--- src/main/resources/static/compute.js
+++ src/main/resources/static/compute.js
@@ -4,7 +4,23 @@ function computeValue() {
   let operator = document.getElementById("operator").value;
   let resultField = document.getElementById("result");
 
+  if (!operand1 || !operand2 || isNaN(operand1) || isNaN(operand2)) {
+    resultField.value = "Please enter numbers";
+    return;
+  }
+
+  if (operand2 == 0 && operator === "divide") {
+    resultField.value = "Zero division disallowed";
+    return;
+  }
+
   fetch(`api/${operator}/${operand1}/${operand2}`)
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      return "Error";
+    } else {
+      return response.json();
+    }
+  })
   .then((result) => resultField.value = result);
 }
